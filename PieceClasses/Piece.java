4bb5ef1394d203af9442fe22a41fc93462d29b68
JesseLemeer <j.lemeer@student.maastrichtuniversity.nl>
2023-09-20 11:40:23 +0200

    Changes

--- PieceClasses/Piece.java
+++ PieceClasses/Piece.java
@@ -4,9 +4,17 @@ public class Piece {
     private final int rank;
     private int[] position = new int[2];
     private boolean isDead;
+    
 
     public Piece(int rank) {
         this.rank = rank;
+    }
+
+    public boolean isMovable(){
+    if(rank==11||rank==12||rank==0){//flag, bomb, empty
+        return false;
+    }
+        return true;
     }
 
     public int getRank() {
