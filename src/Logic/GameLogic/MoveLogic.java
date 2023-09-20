fc7120c4a11d17dee5ad31391248ef53c240aa1d
KonradRtz <115259866+KonradRtz@users.noreply.github.com>
2023-09-20 15:38:30 +0200

    can move method added to move logic

--- src/Logic/GameLogic/MoveLogic.java
+++ src/Logic/GameLogic/MoveLogic.java
@@ -1,5 +1,66 @@
 package GameLogic;
 
+import PieceLogic.Piece;
+
 public class MoveLogic {
-    
+
+    public boolean canMove(Piece piece, int[] targetPosition, Piece[][] positionArray) {
+        if(piece.getRank() == 12 || piece.getRank() == 11 || piece.getRank() == -1) {
+            return false;
+        } else if (positionArray[piece.getPosition()[targetPosition[0]]][piece.getPosition()[targetPosition[1]]] != null) {
+            return false;
+        } else if (piece.getPosition()[0] != targetPosition[0] || piece.getPosition()[1] != targetPosition[1]) {
+            return false;
+        } else if (piece.getRank() == 2 && !canScoutMove(piece, targetPosition, positionArray)) {
+            return false;
+        } else if (((piece.getPosition()[0] == targetPosition[0]) && Math.abs(piece.getPosition()[1] -targetPosition[1]) > 1) || ((piece.getPosition()[1] == targetPosition[1]) && Math.abs(piece.getPosition()[0] - targetPosition[0]) > 1)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean canScoutMove(Piece piece, int[] targetPosition, Piece[][] positionArray) {
+        if(piece.getPosition()[0] == targetPosition[0]) {
+            if(targetPosition[1]  > piece.getPosition()[1]) {
+                for (int i = piece.getPosition()[1]; i <= targetPosition[1]; i++) {
+                    if(positionArray[0][i] != null) {
+                        return false;
+                    }
+                }
+                return true;
+            } else if(targetPosition[1]  < piece.getPosition()[1]) {
+                for (int i = piece.getPosition()[1]; i >= targetPosition[1]; i--) {
+                    if(positionArray[0][i] != null) {
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                System.out.println("Sth went wrong in CanScoutMove");
+                return false;
+            }
+        } else if(piece.getPosition()[1] == targetPosition[1]) {
+           if(targetPosition[0]  > piece.getPosition()[0]) {
+                for (int i = piece.getPosition()[0]; i <= targetPosition[0]; i++) {
+                    if(positionArray[i][1] != null) {
+                        return false;
+                    }
+                }
+                return true;
+            } else if(targetPosition[1]  < piece.getPosition()[1]) {
+                for (int i = piece.getPosition()[0]; i >= targetPosition[0]; i--) {
+                    if(positionArray[i][1] != null) {
+                        return false;
+                    }
+                } 
+                return true;
+            } else {
+                System.out.println("Sth went wrong in CanScoutMove");
+                return false;
+            }
+        } else {
+            System.out.println("Error in the Movement Logic");
+            return false;
+        } 
+    }
 }
