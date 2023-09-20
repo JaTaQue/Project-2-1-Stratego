769fcf55d1127dc33b9c888e1926265c4bfdb405
Irina Ruzavina <96889981+ir1112@users.noreply.github.com>
2023-09-20 12:12:34 +0200

    Revert "Changes"
    This reverts commit 4bb5ef1394d203af9442fe22a41fc93462d29b68.

--- src/Logic/Pieces/PiecesCreator.java
+++ src/Logic/Pieces/PiecesCreator.java
@@ -4,20 +4,6 @@ public class PiecesCreator {
     private int[] pieceInfos = {1, 8, 5, 4, 4, 4, 3 , 2, 1, 1, 1, 6}; //gives us the amount of figures we need prelast index is flag,
     //last index is bombs
 
-    // Rank 0 = Empty
-    // Rank 1 = Spy
-    // Rank 2 = Scout
-    // Rank 3 = Miner
-    // Rank 4 = Sergeant
-    // Rank 5 = Lieutenant
-    // Rank 6 = Captain
-    // Rank 7 = Major
-    // Rank 8 = Colonel
-    // Rank 9 = General
-    // Rank 10 = Marshal
-    // Rank 11 = Flag
-    // Rank 12 = Bomb
-
     public Piece[][] createPieces() {
         Piece[][] newPieces = new Piece[pieceInfos.length][];
         for (int i = 0; i < pieceInfos.length; i++) {
