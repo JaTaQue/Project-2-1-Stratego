99b0ed77b8018650f0bc51809069fa2ea7c106cb
KonradRtz <115259866+KonradRtz@users.noreply.github.com>
2023-09-21 11:01:26 +0200

    added an abstract class for the Player

---
+++ src/Logic/PlayerClasses/PlayerInterface.java
@@ -0,0 +1,45 @@
+package PlayerClasses;
+
+import java.util.ArrayList;
+
+import PieceLogic.Piece;
+
+public abstract class PlayerInterface {
+    private final String COlOR;
+    private Piece[][] pieces;
+    private ArrayList<Piece> deadPieces = new ArrayList<>();
+    private ArrayList<Piece> availablePieces = new ArrayList<>(); //is it needed
+    private boolean isWinner;
+
+    public ArrayList<Piece> getDeadPieces() {
+        return this.deadPieces;
+    }
+
+    public ArrayList<Piece> getAvailablePieces() {
+        return this.availablePieces;
+    }
+
+    public Piece[][] getPieces() {
+        return this.pieces;
+    }
+
+    public String getColor() {
+        return this.COlOR;
+    }
+
+    public boolean isWinner() {
+        return this.isWinner;
+    }
+
+    public void addDeadPiece(Piece deadPiece) {
+        this.deadPieces.add(deadPiece);
+    }
+
+    public void addAvailablePiece(Piece availablePiece) {
+        this.availablePieces.add(availablePiece);
+    }
+
+    public void setWinner() {
+        this.isWinner = false;
+    }
+}
