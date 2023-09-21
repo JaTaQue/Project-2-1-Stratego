d0b19cba7c6e8db23101badd7ec2978a36eb5b94
KonradRtz <115259866+KonradRtz@users.noreply.github.com>
2023-09-21 11:07:36 +0200

    some morenthings added to HumanPlayer

--- src/Logic/PlayerClasses/HumanPlayer.java
+++ src/Logic/PlayerClasses/HumanPlayer.java
@@ -1,5 +1,54 @@
 package PlayerClasses;
 
+import java.util.ArrayList;
+
+import PieceLogic.Piece;
+
 public class HumanPlayer extends PlayerInterface {
+    private  String COlOR;
+    private Piece[][] pieces;
+    private ArrayList<Piece> deadPieces = new ArrayList<>();
+    private ArrayList<Piece> availablePieces = new ArrayList<>(); //is it needed
+    private boolean isWinner;
+
+    @Override
+    public ArrayList<Piece> getDeadPieces() {
+        return this.deadPieces;
+    }
+
+    @Override
+    public ArrayList<Piece> getAvailablePieces() {
+        return this.availablePieces;
+    }
+
+    @Override
+    public Piece[][] getPieces() {
+        return this.pieces;
+    }
+
     
+    @Override
+    public String getColor() {
+        return this.COlOR;
+    }
+
+    @Override
+    public boolean isWinner() {
+        return this.isWinner;
+    }
+
+    @Override
+    public void addDeadPiece(Piece deadPiece) {
+        this.deadPieces.add(deadPiece);
+    }
+
+    @Override
+    public void addAvailablePiece(Piece availablePiece) {
+        this.availablePieces.add(availablePiece);
+    }
+
+    @Override
+    public void setWinner() {
+        this.isWinner = false;
+    }
 }
