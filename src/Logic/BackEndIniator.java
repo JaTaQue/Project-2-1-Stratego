e87cd97c4a30cf478b0db6e6888712fab5511e2f
Irina Ruzavina <96889981+ir1112@users.noreply.github.com>
2023-09-28 12:23:42 +0200

    merging konrad continued

--- src/Logic/BackEndIniator.java
+++ src/Logic/BackEndIniator.java
@@ -7,7 +7,7 @@ public class BackEndIniator {
     private Piece[][] board = new Piece[10][10];
 
     public HumanPlayer createHumanPlayer (String color) {
-        HumanPlayer A = new HumanPlayer();
+        HumanPlayer A = new HumanPlayer(color);
         A.setColor(color);
         A.initializePieces(color);
         return A;
