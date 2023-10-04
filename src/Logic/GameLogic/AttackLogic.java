50856c28642419c9970ebdf39e89e117fa52cdca
KonradRtz <115259866+KonradRtz@users.noreply.github.com>
2023-10-04 12:28:50 +0200

    i hate weird bugs

--- src/Logic/GameLogic/AttackLogic.java
+++ src/Logic/GameLogic/AttackLogic.java
@@ -47,12 +47,106 @@ public class AttackLogic {
         return defenderFigure;
     }
 
-    public static boolean canAttack() {
-        if(attackerFigure.getColor().equals(defenderFigure.getColor()) && (defenderFigure == null || defenderFigure.getRank() == -1)) {
-            return false;
+    public static boolean canAttack(int[] attackerPos,int[] defenderPos, Piece[][] positionArray) {
+        attackerPos = attackerFigure.getPosition();
+        int AttackerXPos = attackerPos[0];
+        int AttackerYPos = attackerPos[1];
+
+        defenderPos = defenderFigure.getPosition();
+        int DefenderXPos = defenderPos[0];
+        int DefenderYPos = defenderPos[1];
+
+        
+        //all other pieces 
+        if (attackerFigure.getRank() != 2 && attackerFigure.getColor() != defenderFigure.getColor()) 
+        {
+            
+            if(AttackerXPos == DefenderXPos)
+            {
+                    if(DefenderYPos - AttackerYPos > 0)
+                    {
+                        AttackerYPos++; // if the X cordiante are the same then it will go towards the piece in the Y axis
+                    }
+                    else {
+                        AttackerYPos--;
+                    }
+                    if(AttackerYPos == DefenderYPos)
+                        {
+                             return true;
+                        }   
+            }
+            else if(AttackerYPos == DefenderYPos)
+            {
+                    if(DefenderYPos - AttackerYPos > 0)
+                    {
+                        AttackerXPos++; // if the X cordiante are the same then it will go towards the piece in the Y axis
+                    }
+                    else {
+                        AttackerXPos--;
+                    }
+                    if(AttackerXPos == DefenderXPos)
+                        {
+                            return true;
+                        }
+            }
+            
         }
-        return true;
-    }
+
+        //Scout 
+        if (attackerFigure.getRank() == 2 && attackerFigure.getColor() != defenderFigure.getColor())
+        {
+            if(AttackerXPos == DefenderXPos)
+            {
+                for (int i=0; i < Math.abs(AttackerYPos-DefenderYPos); i++) // the loop is for the number od steps it takes
+                {
+                    if(DefenderYPos - AttackerYPos > 0)
+                    {
+                        AttackerYPos++; // if the X cordiante are the same then it will go towards the piece in the Y axis
+                    }
+                    else {
+                        AttackerYPos--;
+                    }
+                   
+                    if(positionArray[AttackerXPos][AttackerYPos] == null)
+                        {
+                            continue; // moves in the direction where there is no pice (null) until it reaches the piece 
+                        }
+                    else if(positionArray[AttackerXPos][AttackerYPos] != null)
+                        {
+                            break;
+                        }
+                    else if(AttackerYPos == DefenderYPos){
+                        return true;
+                    }
+                } 
+            }
+            else if(AttackerYPos == DefenderYPos)
+            {
+                for (int i=0; i < Math.abs(AttackerXPos-DefenderXPos); i++)
+                {
+                    if(DefenderXPos - AttackerXPos > 0)
+                    {
+                        AttackerYPos++; // if the X cordiante are the same then it will go towards the piece in the Y axis
+                    }
+                    else {
+                        AttackerXPos--;
+                    } 
+                    if(positionArray[AttackerXPos][AttackerYPos] == null)
+                        {
+                            continue;
+                        }
+                    else if(positionArray[AttackerXPos][AttackerYPos] != null)
+                        {
+                            break;
+                        }
+                    else if(AttackerXPos == DefenderXPos){
+                        return true;
+                    }
+                } 
+            }
+        } 
+        return false;
+    }
 
     public static void battle() {
         if (attackerFigure.getRank() == 1 && defenderFigure.getRank() == 10) {
