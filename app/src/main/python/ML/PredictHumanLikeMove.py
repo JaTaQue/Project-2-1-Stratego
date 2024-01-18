71086aa7fa7029d84d9651c6fa3595491a42db66
KonradRtz <k.retzlaff@student.maastrichtuniversity.nl>
2024-01-18 13:10:16 +0100

    Fixed tensorflow prediction class

--- app/src/main/python/ML/PredictHumanLikeMove.py
+++ app/src/main/python/ML/PredictHumanLikeMove.py
@@ -12,15 +12,17 @@ class PredictHumanLikeMove:
         model = tf.keras.models.load_model(model_path)
         
         # Build the full path to the CSV file
-        data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingDataKonnie.csv')
+        data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'PredictTest.csv')
 
         # Now read the CSV file
         data = pd.read_csv(data_file)
+        data = data.drop('Class', axis=1)
         
         # Make predictions using the loaded model
         prediction = model.predict(data)
         print(type(prediction))
+        print(prediction[1000][1])
         
-        return prediction
+        return prediction[0][1]
 
 PredictHumanLikeMove.predict()
