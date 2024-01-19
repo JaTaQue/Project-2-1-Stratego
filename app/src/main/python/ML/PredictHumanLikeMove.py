89bb74ad7461c84a68cdf189d271a447debedb3c
KonradRtz <k.retzlaff@student.maastrichtuniversity.nl>
2024-01-19 13:51:52 +0100

    added a test method for evaluation

--- app/src/main/python/ML/PredictHumanLikeMove.py
+++ app/src/main/python/ML/PredictHumanLikeMove.py
@@ -26,10 +26,37 @@ class PredictHumanLikeMove:
         
         # Make predictions using the loaded model
         prediction = self.model.predict(data)
+        print(prediction.tolist())
         print("------------------------------------------------------------------------------------------------------\n")
         return prediction[0][1]
 
+
+    def test(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Build the full path to the CSV file
+        data_file = os.path.join(script_dir, 'ANNTrainingData.csv')
+
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+
+        # Now read the CSV file
+        data = pd.read_csv(data_file)
+
+        # Separate the features and target variables
+        X = data.drop('Class', axis=1)
+        y = data['Class']
+
+        # Split the data into training and test sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.34, random_state=42)
+
+        self.model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        train_loss, train_accuracy = self.model.evaluate(X_train, y_train)
+        test_loss, test_accuracy = self.model.evaluate(X_test, y_test)
+        print("Training Accuracy: {:.2f}%".format(train_accuracy * 100))
+        print("Test Accuracy: {:.2f}%".format(test_accuracy * 100))
+
 phlm = PredictHumanLikeMove()
-phlm.predict()
-phlm.predict()
-phlm.predict()
+phlm.test()
